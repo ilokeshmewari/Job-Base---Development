462465222f214ab813f8dca2ee04f7b265b03100
Đeēpãk Ťhăkűr <dt7252058169@gmail.com>
2025-02-20 12:20:13 +0530

    MakeYouCode Updated

--- src/components/MakeYouCode.tsx
+++ src/components/MakeYouCode.tsx
@@ -5,19 +5,19 @@ import Link from "next/link";
 
 export default function AdBanner() {
   return (
-    <div className="bg-white p-6 flex flex-col h-full justify-around items-center text-center w-full mx-auto">
+    <div className="bg-slate-200 p-6 flex flex-col h-full justify-around items-center text-center w-full mx-auto">
       {/* Logo */}
       <Image
         src="https://bikasjfxwokxjqwledqb.supabase.co/storage/v1/object/public/codeews/logo.png"
         alt="MakeYouCode Logo"
-        width={104}
-        height={104}
+        width={250}
+        height={250}
         unoptimized
       />
 
       {/* Description */}
-      <h1 className="text-2xl font-bold text-blue-900 mt-4">MakeYouCode</h1>
-      <p className="text-sm text-gray-700 mt-2">
+      {/* <h1 className="text-2xl font-bold text-blue-900 mt-4">MakeYouCode</h1> */}
+      <p className="text-4xl text-gray-700 mt-2 text-left">
         Learn, Practice, and Master Coding with DSA & Competitive Programming.
       </p>
 
@@ -25,7 +25,7 @@ export default function AdBanner() {
       <Link
         href="https://makeyoucode.tech"
         target="_blank"
-        className="mt-4 bg-[#EB5B00] hover:bg-orange-600 text-white font-semibold px-5 py-2 rounded-full shadow-md transition"
+        className="mt-4 bg-[#EB5B00] hover:bg-orange-500 text-white font-semibold px-5 py-2 rounded-full shadow-md transition"
       >
         Visit MakeYouCode
       </Link>
