daffbe265d12a0799601dd1cb7f3e778e7e42916
ilokeshmewari <lokeshsinghmewari@gmail.com>
2025-02-21 16:13:37 +0530

    code pushed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,14 @@
-"use client";
-import { useEffect } from "react";
 import Footer from "@/components/Footer";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  useEffect(() => {
-    if ("serviceWorker" in navigator) {
-      navigator.serviceWorker
-        .register("/B2boWl.js") // Use a local service worker file
-        .then((registration) => {
-          console.log("Ad Service Worker registered:", registration);
-        })
-        .catch((error) => {
-          console.error("Ad Service Worker registration failed:", error);
-        });
-    }
-  }, []);
+
 
   return (
     <html lang="en">
       <head>
+      <meta name="monetag" content="bf541f80b3e644fc4b770690cbffe5e2" />
         <title>Job Base</title>
         {/* Ad Manager Script */}
         <script
@@ -28,6 +16,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
           src="https://js.wpadmngr.com/static/adManager.js"
           data-admpid="295943"
         ></script>
+
       </head>
       <body>
         <div className="flex flex-col min-h-screen">
