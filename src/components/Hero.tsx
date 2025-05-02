bd097d59c94d760786ada95a58aa0999906307d0
ilokeshmewari <lokeshsinghmewari@gmail.com>
2025-05-02 21:10:59 +0530

    added new rrev link

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -104,7 +104,7 @@ export default function HeroSection() {
         <a href="https://x.com/jobbase02" target="_blank" rel="noopener noreferrer">
           <Twitter className="w-6 h-6 text-black hover:text-gray-800" />
         </a>
-        <a href="https://chat.whatsapp.com/DXZJsmey0UlCqVdD2ztEbK" target="_blank" rel="noopener noreferrer">
+        <a href="https://chat.whatsapp.com/HpM7OdWPlTs0A8Sm913WKF" target="_blank" rel="noopener noreferrer">
           <MessageCircleMore className="w-6 h-6 text-green-500 hover:text-green-600" />
         </a>
         <a href="https://t.me/jobbase_25" target="_blank" rel="noopener noreferrer">
